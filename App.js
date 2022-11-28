ea9be9ca74cff3d8be84d0d29437b06a967c8550
marisalen <mlenarduzzi1@my.bcit.ca>
2022-11-28 11:04:16 -0800

    commit

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { getFirestore } from "firebase/firestore";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/Home';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATtzu6yvZBsnAnR6h3u2_2FNoFoyQSWWQ",
+  authDomain: "firestore1-c9776.firebaseapp.com",
+  projectId: "firestore1-c9776",
+  storageBucket: "firestore1-c9776.appspot.com",
+  messagingSenderId: "732428271213",
+  appId: "1:732428271213:web:bb622c0ea6f7d78db5c964"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const Stack = createNativeStackNavigator();
+const db = getFirestore(app);
+const analytics = getAnalytics(app);
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+     <Stack.Navigator>
+       <Stack.Screen name="Home" component={HomeScreen}/>
+     </Stack.Navigator>
+   </NavigationContainer>
   );
 }
 
